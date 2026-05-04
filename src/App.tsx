ad1092448e6d465b5fb1b27b7424ac31b991f80c
Aman3008akn <67892829+Aman3008akn@users.noreply.github.com>
2026-05-04 21:57:33 +0530

    Add CBSE Class 10th toppers 2024-2025 page

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ const ContactPage = lazy(() => import("./pages/ContactPage"));
 const ShippingPage = lazy(() => import("./pages/ShippingPage"));
 const ReturnsPage = lazy(() => import("./pages/ReturnsPage"));
 const FAQPage = lazy(() => import("./pages/FAQPage"));
+const CbseToppersPage = lazy(() => import("./pages/CbseToppersPage"));
 
 function AppContent() {
   const { isMaintenanceMode } = useMaintenance();
@@ -67,6 +68,7 @@ function AppContent() {
             <Route path="/shipping" element={<ShippingPage />} />
             <Route path="/returns" element={<ReturnsPage />} />
             <Route path="/faq" element={<FAQPage />} />
+            <Route path="/cbse-class-10th-toppers-2024-2025" element={<CbseToppersPage />} />
             <Route path="/admin/*" element={<AdminDashboard />} />
           </Routes>
         </Suspense>
